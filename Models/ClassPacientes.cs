b554faa367842b10d526cc02961e7d9d37034b96
Alexandre Morato <alexandre.morato@gmail.com>
2021-05-18 00:01:18 -0300

    Commit 2 - Inclusão da Classe Pacientes Ajuste da connection string no webconfig

---
+++ Models/ClassPacientes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTasyFake.Models
+{
+    public class ClassPacientes
+    {
+        public int Idpaciente { get; set; }
+        public int iNumeroProntuario { get; set; }
+        public string sNomePaciente { get; set; }
+        public DateTime dtNascPaciente { get; set; }
+        public string sCPFPaciente { get; set; }
+        public string sEmailPaciente { get; set; }
+        public DateTime dtCadastro { get; set; }
+        public string sCodUltimoAtendimento { get; set; }
+        public DateTime dtUltimoAtendimento { get; set; }
+        public int idEstabelecimento { get; set; }
+
+
+    }
+}
